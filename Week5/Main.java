6eaa2662fb6d8d1cfa135f2c4094afd15efbecdf
Khang <doangiang2612@gmail.com>
2022-05-23 09:29:54 +0700

    ktra

--- Week5/Main.java
+++ Week5/Main.java
@@ -48,37 +48,37 @@ public class Main {
         // }
 
         ////////////////////// insert
-        // try{
-        // Statement st = conn.createStatement();
-        //
-        // // note that i'm leaving "date_created" out of this insert statement
-        // st.executeUpdate("INSERT INTO books (id, title, author,price, qty) "
-        // +"VALUES (1004, 'Doraemon', 'Mason',25.25, 5)");
-        //
-        // conn.close();
-        // }
-        // catch (Exception e)
-        // {
-        // System.err.println("Got an exception!");
-        // System.err.println(e.getMessage());
-        // }
-        //
+         try{
+         Statement st = conn.createStatement();
+
+         // note that i'm leaving "date_created" out of this insert statement
+         st.executeUpdate("INSERT INTO books (id, title, author,price, qty) "
+         +"VALUES (1005, 'Conan', 'Aoyama Gosho',75.5, 3)");
+
+         conn.close();
+         }
+         catch (Exception e)
+         {
+         System.err.println("Got an exception!");
+         System.err.println(e.getMessage());
+         }
+
         ////////////////////// update
-        String query2 = "UPDATE books SET qty = ? WHERE id = ?";
-
-        PreparedStatement update = null;
-        try {
-            update = conn.prepareStatement(query2);
-
-            update.setInt(1, 20);
-            update.setInt(2, 1004);
-            int row2 = update.executeUpdate();
-            if (row2 != 0) {
-                System.out.println("Cập nhật thành công " + row2);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+//        String query2 = "UPDATE books SET qty = ? WHERE id = ?";
+//
+//        PreparedStatement update = null;
+//        try {
+//            update = conn.prepareStatement(query2);
+//
+//            update.setInt(1, 20);
+//            update.setInt(2, 1004);
+//            int row2 = update.executeUpdate();
+//            if (row2 != 0) {
+//                System.out.println("Cập nhật thành công " + row2);
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
         //
         System.out.println("-----------------------");
         ////////////////////// select
